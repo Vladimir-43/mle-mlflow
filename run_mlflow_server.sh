71c85eb385a39724904e8ac7b36ca5574ce04cd3
Vladimir-43 <mle-user@epdloo3l6jig2ehskh61.auto.internal>
2025-12-24 18:29:22 +0000

    запустили  mlflow, записали артефакты, см. ipynb файлы

--- run_mlflow_server.sh
+++ run_mlflow_server.sh
@@ -1,3 +1,5 @@
+#export $(cat .env | xargs)
+
 export MLFLOW_S3_ENDPOINT_URL=https://storage.yandexcloud.net
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
